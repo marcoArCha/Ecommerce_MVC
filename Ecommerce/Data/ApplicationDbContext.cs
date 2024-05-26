c20f99034aec0285ec9c520e665bac5147b08a6c
Marco Arias <marcoarcha@hotmail.com>
2024-05-26 07:32:38 -0600

    Services added to container in Program.cs

---
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    //Mainly configuration
+    public class ApplicationDbContext : DbContext
+    {
+        //Pass connection string
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
