2e5e33195c850fc44723efd970c115edb0d4cae2
Marco Arias <marcoarcha@hotmail.com>
2024-05-27 06:43:03 -0600

    Data retrieving from Category table

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Models.Category> objCategoryList = _db.Categories.ToList();   
+            return View(objCategoryList);
         }
     }
 }
