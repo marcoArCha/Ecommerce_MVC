6eb254d80e4e02888deb895e3fe92b970470aff0
Marco Arias <marcoarcha@hotmail.com>
2024-05-31 06:14:15 -0600

    Create post method added for Category model.

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -20,5 +20,17 @@ namespace Ecommerce.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Models.Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            //RedirectToAction looks for the action in the same controller
+            //If the action is in another controller, pass it as a parameter
+            //to RedirectToAction like this:
+            //return RedirectToAction("Index", "ControllerName");
+            return RedirectToAction("Index");
+        }
     }
 }
