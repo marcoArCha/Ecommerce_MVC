c4217e69f1b26b451cd520637bff2fe21bb3cb5d
Marco Arias <marcoarcha@hotmail.com>
2024-06-01 06:21:28 -0600

    Server side validation for Category model

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -21,16 +21,22 @@ namespace Ecommerce.Controllers
             return View();
         }
 
+        //Server side validation takes place in the controller
         [HttpPost]
         public IActionResult Create(Models.Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            //RedirectToAction looks for the action in the same controller
-            //If the action is in another controller, pass it as a parameter
-            //to RedirectToAction like this:
-            //return RedirectToAction("Index", "ControllerName");
-            return RedirectToAction("Index");
-        }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                //RedirectToAction looks for the action in the same controller
+                //If the action is in another controller, pass it as a parameter
+                //to RedirectToAction like this:
+                //return RedirectToAction("Index", "ControllerName");
+                return RedirectToAction("Index");
+            }
+            return View();
+        }   
     }
 }
