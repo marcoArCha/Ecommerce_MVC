fd7255c732312ccc28c0bd27604da7aeba0310b8
Marco Arias <marcoarcha@hotmail.com>
2024-06-04 06:53:12 -0600

    Edit action in Category Controller

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.Data;
+using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Controllers
@@ -44,5 +45,31 @@ namespace Ecommerce.Controllers
             }
             return View();
         }   
+
+        public IActionResult Edit(int? id)
+        {
+            if(id== null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Diferent ways of retreving from a DB
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=> u.Id == id).FirstOrDefault();
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit()
+        {
+            return View();
+        }
     }
 }
